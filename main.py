86f91a029d27f7e5a76c222011d898e36a63474f
hugo Lancery <hugo.lancery@hotmail.com>
2023-11-03 10:14:23 +0100

    upload

--- main.py
+++ main.py
@@ -1,11 +1,41 @@
+import os
+from pathlib import Path
 import sys
-import json
-import ast
 
-data_to_pass_back = "received :"
+from PySide6.QtCore import QUrl
+from PySide6.QtWidgets import QApplication, QVBoxLayout, QWidget
+from PySide6.QtWebEngineWidgets import QWebEngineView
 
-input = sys.argv[1]
-output = data_to_pass_back + input
 
-print(output)
-sys.stdout.flush()
+CURRENT_DIRECTORY = Path(__file__).resolve().parent
+
+
+class webView(QWidget):
+    def __init__(self):
+        super(webView, self).__init__()
+        
+
+        filename = os.fspath(CURRENT_DIRECTORY / "index_py.html")
+        url = QUrl.fromLocalFile(filename)
+        
+        self.webV = QWebEngineView()
+        def test1(percent):
+            print(percent)
+        self.webV.loadProgress.connect(test1)
+        def test2(success):
+            print(success)
+        self.webV.loadFinished.connect(test2)
+        self.webV.load(url)
+
+        layout = QVBoxLayout(self)
+        layout.addWidget(self.webV)
+
+        print (self.size())
+
+if __name__ == "__main__":
+    app = QApplication([])
+
+    web = webView()
+    web.show()
+
+    sys.exit(app.exec())
--- sketch3.js
+++ sketch3.js
@@ -2,141 +2,130 @@ let satellite_val = 0;
 let my_object;
 let data_sat;
 
-var s3 = function (sketch) {
-  sketch.preload = function () {
-    data_sat = sketch.loadJSON("data.json");
-    img = sketch.loadImage("map_earth.jpg");
-  };
-  sketch.setup = function () {
-    let canvas = sketch.createCanvas(
-      sketch.windowWidth - (360 + sketch.windowWidth * 0.01),
-      sketch.windowHeight,
-      sketch.WEBGL
-    );
-    canvas.position(360 + sketch.windowWidth * 0.01, 0);
-    sel = sketch.createSelect();
-    for (let index = 0; index < 100; index++) {
-      sel.position(20, 20);
-      sel.option(index + ":" + data_sat[index].OBJECT_NAME);
-      sel.changed(mySelectEvent_listbox);
-    }
-    my_object = new Orbit(data_sat[0]);
-    my_object.total();
-    sketch.angleMode(sketch.DEGREES);
-  };
+function preload() {
+  data_sat = loadJSON("data.json");
+  img = loadImage("map_earth.jpg");
+}
 
-  function mySelectEvent_listbox() {
-    let item = sel.selected();
-    let val = 0;
-    for (let index = 0; index < item.length; index++) {
-      if (item[index] == ":") {
-        val = parseInt(item.slice(0, index));
-      }
-    }
-    satellite_val = val;
-    my_object = new Orbit(data_sat[satellite_val]);
+function setup() {
+  let canvas = createCanvas(windowWidth, windowHeight, WEBGL);
+  sel = createSelect();
+  sel.id(12);
+  for (let index = 0; index < 100; index++) {
+    sel.option(index + ": " + data_sat[index].OBJECT_NAME);
+    sel.changed(mySelectEvent_listbox);
   }
+  my_object = new Orbit(data_sat[0]);
+  my_object.total();
+  angleMode(DEGREES);
+}
 
-  sketch.draw = function () {
-    my_object = new Orbit(data_sat[satellite_val]);
-    my_object.total();
-
-    sketch.clear();
-    sketch.orbitControl(1, 1, 1);
-    sketch.scale(0.03);
-
-    sketch.push();
-    sketch.fill(0, 100, 100);
-    sketch.texture(img);
-    sketch.rotateX(0);
-    sketch.rotateY(-180);
-    sketch.rotateZ(0);
-    sketch.sphere(6371, 50, 50);
-    sketch.pop();
-
-    sketch.push();
-    sketch.stroke(255, 0, 255);
-    sketch.strokeWeight(3);
-    sketch.beginShape(sketch.POINTS);
-    for (let index = 0; index < my_object.R[0].length; index++) {
-      sketch.vertex(
-        (Math.cos((my_object.lat[index] * Math.PI) / 180) *
-          Math.sin((my_object.lon[index] * Math.PI) / 180) *
-          my_object.alt[index]) /
-          1000,
-        (-Math.sin((my_object.lat[index] * Math.PI) / 180) * my_object.alt[0]) /
-          1000,
-        (Math.cos((my_object.lat[index] * Math.PI) / 180) *
-          Math.cos((my_object.lon[index] * Math.PI) / 180) *
-          my_object.alt[index]) /
-          1000
-      );
+function mySelectEvent_listbox() {
+  let item = sel.selected();
+  let val = 0;
+  for (let index = 0; index < item.length; index++) {
+    if (item[index] == ":") {
+      val = parseInt(item.slice(0, index));
     }
-    sketch.endShape();
-    sketch.pop();
+  }
+  satellite_val = val;
+  my_object = new Orbit(data_sat[satellite_val]);
+}
 
-    sketch.push();
-    sketch.stroke(0, 255, 0);
-    sketch.strokeWeight(10);
-    sketch.beginShape(sketch.POINTS);
-    sketch.vertex(
-      (Math.cos((my_object.lat[0] * Math.PI) / 180) *
-        Math.sin((my_object.lon[0] * Math.PI) / 180) *
-        my_object.alt[0]) /
+function draw() {
+  my_object = new Orbit(data_sat[satellite_val]);
+  my_object.total();
+
+  clear();
+  orbitControl(1, 1, 1);
+  scale(0.03);
+
+  push();
+  fill(0, 100, 100);
+  texture(img);
+  rotateX(0);
+  rotateY(-180);
+  rotateZ(0);
+  sphere(6371, 50, 50);
+  pop();
+
+  push();
+  stroke(255, 0, 255);
+  strokeWeight(3);
+  beginShape(POINTS);
+  for (let index = 0; index < my_object.R[0].length; index++) {
+    vertex(
+      (Math.cos((my_object.lat[index] * Math.PI) / 180) *
+        Math.sin((my_object.lon[index] * Math.PI) / 180) *
+        my_object.alt[index]) /
         1000,
-      (-Math.sin((my_object.lat[0] * Math.PI) / 180) * my_object.alt[0]) / 1000,
-      (Math.cos((my_object.lat[0] * Math.PI) / 180) *
-        Math.cos((my_object.lon[0] * Math.PI) / 180) *
-        my_object.alt[0]) /
+      (-Math.sin((my_object.lat[index] * Math.PI) / 180) * my_object.alt[0]) /
+        1000,
+      (Math.cos((my_object.lat[index] * Math.PI) / 180) *
+        Math.cos((my_object.lon[index] * Math.PI) / 180) *
+        my_object.alt[index]) /
         1000
     );
-    sketch.endShape();
-    sketch.pop();
+  }
+  endShape();
+  pop();
 
-    sketch.push();
-    sketch.strokeWeight(3);
-    sketch.stroke(255, 0, 0); //RED
-    sketch.line(0, 0, 0, 10000, 0, 0);
-    sketch.stroke(0, 255, 0); //GREEN
-    sketch.line(0, 0, 0, 0, 10000, 0);
-    sketch.stroke(0, 0, 255); //BLUE
-    sketch.line(0, 0, 0, 0, 0, 10000);
-    sketch.pop();
+  push();
+  stroke(0, 255, 0);
+  strokeWeight(10);
+  beginShape(POINTS);
+  vertex(
+    (Math.cos((my_object.lat[0] * Math.PI) / 180) *
+      Math.sin((my_object.lon[0] * Math.PI) / 180) *
+      my_object.alt[0]) /
+      1000,
+    (-Math.sin((my_object.lat[0] * Math.PI) / 180) * my_object.alt[0]) / 1000,
+    (Math.cos((my_object.lat[0] * Math.PI) / 180) *
+      Math.cos((my_object.lon[0] * Math.PI) / 180) *
+      my_object.alt[0]) /
+      1000
+  );
+  endShape();
+  pop();
+  push();
+  strokeWeight(3);
+  stroke(0, 0, 255); //BLUE
+  line(0, 0, 0, 0, 10000, 0);
+  stroke(255, 0, 0); //RED
+  line(0, 0, 0, 0, -10000, 0);
 
-    document.getElementById("param").innerHTML =
-      "Period: " +
-      sketch.round(my_object.PERIOD, 2) +
-      " min <br />Latitude: " +
-      sketch.round(my_object.lat[0], 2) +
-      "° <br />Longitude: " +
-      sketch.round(my_object.lon[0], 2) +
-      " ° <br />Epoch: " +
-      my_object.EPOCH +
-      "<br />GMST: " +
-      sketch.round(my_object.GMST) +
-      " °<br />Inclination: " +
-      sketch.round((my_object.INCLINATION * 180) / Math.PI, 2) +
-      " °";
+  document.getElementById("text_parameters").innerHTML =
+    "Period: " +
+    round(my_object.PERIOD, 2) +
+    " min <br />Latitude: " +
+    round(my_object.lat[0], 2) +
+    "° <br />Longitude: " +
+    round(my_object.lon[0], 2) +
+    " ° <br />Epoch: " +
+    my_object.EPOCH +
+    "<br />GMST: " +
+    round(my_object.GMST) +
+    " °<br />Inclination: " +
+    round((my_object.INCLINATION * 180) / Math.PI, 2) +
+    " °";
 
-    let canvas = document.getElementById("myCanvas");
-    let ctx = canvas.getContext("2d");
-    ctx.beginPath();
-    ctx.clearRect(0, 0, 360, 180);
-    ctx.closePath();
+  let canvas = document.getElementById("plot_groundtrack");
+  let ctx = canvas.getContext("2d");
+  ctx.beginPath();
+  ctx.clearRect(0, 0, 360, 180);
+  ctx.closePath();
 
-    ctx.fillStyle = "#ff00ff";
-    ctx.beginPath();
-    for (let index = 0; index < my_object.R[0].length; index++) {
-      ctx.fillRect(my_object.lon[index] + 180, 90 - my_object.lat[index], 1, 1);
-    }
-    ctx.closePath();
+  ctx.fillStyle = "#ff00ff";
+  ctx.beginPath();
+  for (let index = 0; index < my_object.R[0].length; index++) {
+    ctx.fillRect(my_object.lon[index] + 180, 90 - my_object.lat[index], 1, 1);
+  }
+  ctx.closePath();
 
-    ctx.beginPath();
-    ctx.arc(my_object.lon[0] + 180, 90 - my_object.lat[0], 4, 0, Math.PI * 2);
-    ctx.fillStyle = "#00ff00";
-    ctx.fill();
-    ctx.closePath();
-  };
-};
-
-new p5(s3);
+  ctx.beginPath();
+  ctx.arc(my_object.lon[0] + 180, 90 - my_object.lat[0], 4, 0, Math.PI * 2);
+  ctx.fillStyle = "#00ff00";
+  ctx.fill();
+  ctx.closePath();
+  console.log(sel.id());
+}
