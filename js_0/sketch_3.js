1385a504f68d7966a4a37df4a89a4835db198054
hugo Lancery <hugo.lancery@hotmail.com>
2023-11-22 21:16:05 +0100

    mods

--- js_0/sketch_3.js
+++
@@ -1,9 +0,0 @@
-function setup() {
-  let canvas = createCanvas(100, 100);
-  canvas.position(0, 0);
-  const myWorker = new Worker("../js_1/sketch_1.js");
-}
-
-function draw() {
-  console.log("doing something");
-}
