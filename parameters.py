bbd1b1980c618d7507619f1de5a9e8234eece08c
hugo Lancery <108919405+LanceryH@users.noreply.github.com>
2024-03-11 01:05:48 +0100

    Update parameters.py

--- parameters.py
+++ parameters.py
@@ -1,8 +1,8 @@
 from random import randint
 PORT = str(randint(1000, 9999))
-URL_0D = "http://127.0.0.1:"+PORT+"/satvision/sketchs/base"  
-URL_3D = "http://127.0.0.1:"+PORT+"/satvision/sketchs/3_d"
-URL_2D = "http://127.0.0.1:"+PORT+"/satvision/sketchs/2_d"
+URL_0D = "http://127.0.0.1:"+PORT+"/sketchs/base"  
+URL_3D = "http://127.0.0.1:"+PORT+"/sketchs/3_d"
+URL_2D = "http://127.0.0.1:"+PORT+"/sketchs/2_d"
 URL_CELESTRAK = "https://celestrak.org/NORAD/elements/gp.php?GROUP=active&FORMAT=json"
 g0 = 9.80665 #m/s
 Omega_e = 6.300387486749 # rad/d
