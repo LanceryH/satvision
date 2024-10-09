2b3a62feff298794f6cf803c120b426880bb6ac2
“Hlancery” <lancery.hugo@outlook.com>
2024-10-09 02:56:06 +0200

    added subprocess

--- main.py
+++ main.py
@@ -3,9 +3,12 @@ from PyQt5.QtWidgets import QApplication, QMainWindow, QSlider, QLabel, QVBoxLay
 from PyQt5.QtCore import Qt
 from socketio import Client
 from interface import *
+import subprocess
 
 if __name__ == '__main__':
+    subprocess.Popen("python server.py")    
     app = QApplication(sys.argv)
     window = MyQtApp()
     window.show()
     sys.exit(app.exec_())
+    
